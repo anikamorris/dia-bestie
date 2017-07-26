16e707ed947edbc20551eb5e2e9dd4e0732ce613
anikamorris <anikamorris@students.berkeley.net>
2017-07-26 13:12:40 -0700

    all outlets hooked up in NoYesCalc

--- Dia-bestie/Dia-bestie/NoYesCalcViewController.swift
+++ Dia-bestie/Dia-bestie/NoYesCalcViewController.swift
@@ -12,6 +12,10 @@ import UIKit
 class NoYesCalcViewController: UIViewController {
     
     @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var carbsTextField: UITextField!
+    @IBOutlet weak var currentBGTextField: UITextField!
+    @IBOutlet weak var findTotalUnitsButton: UIButton!
+    @IBOutlet weak var numberOfUnitsLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +23,9 @@ class NoYesCalcViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func findTotalUnitsButtonTapped(_ sender: UIButton) {
     }
     
 }
